a6ec25e0e13de6f166d69f387ab6e962bbb8b263
Ainnop <atwauinnocent@gmail.com>
2022-08-02 16:30:31 +0300

    1 file added

---
+++ duplicateCount.js
@@ -0,0 +1,12 @@
+function CountDuplicate(text) {
+  const splitString = text.toLowerCase().split('').sort();
+  let results = [];
+
+  for (let i = 0; i < splitString.length; i++) {
+    if (splitString[i] === splitString[i + 1]) {
+      results.push(splitString[i]);
+    }
+  }
+  const setArray = new Set(results);
+  return setArray.size;
+}
